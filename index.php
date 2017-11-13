2621622b640236ddb8eba6ce26accbec42ec3247
wongprom <jimmybackstrom@hotmail.com>
2017-11-13 12:07:45 +0100

    edit index.php

--- index.php
+++ index.php
@@ -2,9 +2,46 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Index Page</title>
 </head>
 <body>
+	<header>
+
+	</header>
+	<nav>
+
+	</nav>
+	<main>
 	
+		
+		<figure>
+			<img src="" alt=""><!--3 CATEGORIES PICTURSE UNDER HERO IMAGE-->
+			<figcapture>KATEGORI</figcapture>
+		</figure>
+
+		<h1>Blogs</h1>
+		
+		
+		<article class="">
+			<button <a href="#">NAMN PÅ KATEGORI</a>></button><!--CATEGORIE TAG-->
+			<h2>BLOG TITLE</h2>
+			<figure>
+				<img src="" alt=""><!--BLOG PICTURE-->
+			</figure>
+			<p class=""></p>
+			<img src="" alt=""><!--USER IMAGE-->
+			<span></span><!--USER NAME-->
+			<time></time>
+		</article>
+	
+	
+	</main>
+	<footer>
+		<button>facebook</button>
+		<button>instagram</button>
+		<button>contact</button>
+		<span>credit</span>
+	</footer>
 </body>
 </html>
