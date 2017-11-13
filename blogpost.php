454401dc9e73e9ff7362d847d7bdf5da706e9f0d
dorisobor <troyamber@live.com>
2017-11-13 13:17:28 +0100

    blogpost page

---
+++ blogpost.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Blogpost</title>
+</head>
+<body>
+	
+	<nav>
+ <!--Menu-->
+	</nav>
+	<main>
+	
+		
+	 <h1>Blogpost</h1>
+		
+		<article class="">
+           <!--CATEGORIE TAG-->
+			<button <a href="#">NAMN PÅ KATEGORI</a>></button>
+			<h2>BLOG TITLE</h2>
+			<figure>
+                <!--BLOG PICTURE-->
+				<img src="" alt="">
+			</figure>
+             <!--USER IMAGE-->
+			<p class=""></p>
+			<img src="" alt="">
+              <!--USER NAME-->
+			<span></span>
+			<time></time>
+		</article>
+	
+	
+	</main>
+	<footer>
+		<button>facebook</button>
+		<button>instagram</button>
+		<button>contact</button>
+		<span>credit</span>
+	</footer>
+</body>
+</html>
