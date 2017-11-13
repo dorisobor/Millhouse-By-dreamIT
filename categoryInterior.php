ca015833349009d1ca0860ee92e4e514c3dcc378
jdagmar <dagmar.j.gustavsson@gmail.com>
2017-11-13 12:54:58 +0100

    added category pages

---
+++ categoryInterior.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Category | Interior</title>
+</head>
+<body>
+
+	<nav>
+
+	</nav>
+
+	<main>
+	
+		<!-- picture representing the category -->
+		<figure>
+			<img src="" alt="">
+		</figure>
+
+		<h1>Interior</h1>
+        <p>Here you can read all about Millhouse exclusive interior details!</p>
+
+
+        <!-- article = blogpost -->
+		
+		<article class="">
+            <!-- tag -->
+			<button <a href="#">Category</a></button>
+			<h2>Blogpost title</h2>
+			<figure>
+				<img src="" alt="">
+			</figure>
+			<p class=""></p>
+            <!-- userimage -->
+			<img src="" alt="">
+			<!-- username -->
+            <span></span>
+            <!-- publish date -->
+			<time></time>
+            <!-- share -->
+            <button></button>
+		</article>
+	
+	</main>
+
+	<footer>
+		<button>facebook</button>
+		<button>instagram</button>
+		<button>contact</button>
+		<span>credit</span>
+	</footer>
+
+</body>
+</html>
