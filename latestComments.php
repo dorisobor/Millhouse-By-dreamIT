22e0fe95bfe73e29b4358f1f3bcc87fd9a1addda
dorisobor <troyamber@live.com>
2017-11-13 22:07:38 +0100

    the latest comments on the profile page

---
+++ latestComments.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Profile Page- latest comments</title>
+</head>
+<body>
+	<header>
+   </header>
+	<nav>
+	<a href="url">logut</a>
+	<!--User icon as a dropdown menue-->
+		<div class="dropdown">
+  <img src="" alt="">
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
+  
+
+	</nav>
+	
+	<div class ="" > 
+	<!--USER IMAGE-->
+       <p class=""></p>
+		<img src="" alt="">
+		<!--edit icon from bootsrap?-->
+		<!--USER NAME-->
+		<span></span>
+		<!--settings icon from bootstrap?-->
+		</div>
+		<!--bootstrap second nav-->
+		<nav>
+		</nav>
+		<!--wrapping container-->
+		<div class ="wrapper">
+		<h2></h2>
+
+		</div> 
+
+		
+		<!--latest comments-->
+		<div  class= "">
+         <article>
+           <!--CATEGORY TAG-->
+			<button <a href="#">NAMN PÅ KATEGORI</a>></button>
+			<h2>BLOG TITLE</h2>
+			<figure>
+			</figure>
+             <!--USER IMAGE-->
+			<p class=""></p>
+			<img src="" alt="">
+              <!--USER NAME-->
+			<span></span>
+			<time></time>
+			<p></p>
+		</article>
+		<button></button>
+		<button></button>
+		</div> 
+	    </div> 
+	</main>
+	<footer>
+		<button>facebook</button>
+		<button>instagram</button>
+		<button>contact</button>
+		<span>credit</span>
+	</footer>
+</body>
+</html>
