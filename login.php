f0ffec3ddd3f1ac0926c08ab97f5d61cfb350c17
jdagmar <dagmar.j.gustavsson@gmail.com>
2017-11-13 13:21:09 +0100

    added login page

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+</head>
+<body>
+
+	<nav>
+
+	</nav>
+
+	<main>
+	
+    <!-- form container -->
+    <div> 
+
+        <h1>Login</h1>
+
+        <form action="" method="">
+            <label for="username" hidden>Write your username or email</label>
+            <input type="text" name="username" value="" placeholder="email or username"/>
+            <label for="username" hidden>Write your password</label>
+            <input type="text" name="username" value="" placeholder="password"/>
+            <input type="submit" value="Log In"/>
+        </form>
+
+        <a href="">create a new account</a>
+
+    </div>
+
+	</main>
+
+	<footer>
+		<button>facebook</button>
+		<button>instagram</button>
+		<button>contact</button>
+		<span>credit</span>
+	</footer>
+    
+</body>
+</html>
