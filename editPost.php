572d7b408cbf80fc4ce59dfb63423966a9dc6766
wongprom <jimmybackstrom@hotmail.com>
2017-11-14 14:52:17 +0100

    add form and checkboxes

--- editPost.php
+++ editPost.php
@@ -13,32 +13,33 @@
 
 	</nav>
 	<main>
-
-        <div class="container">
-            <div class="container__content">
-                <h1>Lorem ipsum dolor sit amet.</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Nihil quos provident deserunt culpa tempore voluptas nostrum 
-                    rerum, architecto nam error saepe nulla alias eveniet beatae! 
-                    Fugiat itaque voluptatum voluptas ipsa?</p>
-            </div>
-
-            <form action="" method="POST" enctype="multypart/form-data">
-                <div class="form-group">
-                <input type="text" name="title">
-                <textarea name="content" id="editor"></textarea>
-                </div>
-                <p>In which category does your post belong in??</p>
-
+        
+        <div class="">
+        <form action="confirmation.php" method="post" enctype="multipart/form-data">
+            <fieldset>
+                <legend> Edit Your Post </legend>
+                <label for="name">Edit Your Post Title</label>
+                 <br /> 
+                <input type="text" name="name" id="name" aria-required="true" />
+                <br />
+                <label for="blog_post"> Write post</label>
+                <br />
+                <textarea id="blog_post" name="blog_post" rows="4" cols="50"></textarea>
+                <br /> 
+                <label for="image"> Image </label>
+                <input type="file" name="image" id="image" />
+                <br />
+                <!--Checkboxes-->
+                <p>In which category does your post belong in?</p>
+                <br />
+                <input type="checkbox" name="vehicle" value="Sunglasses">I have a bike<br>
+                <input type="checkbox" name="vehicle" value="Watches">I have a bike<br>
+                <input type="checkbox" name="vehicle" value="Interiror">I have a bike<br>
                 
-                <input type="submit" value="Skicka">
-            
-
-            
-
-            </form> 
-
-        </div>
+                <input type="submit" value="Upload" />
+            </fieldset>
+        </form>
+    </div>
 		
 		
 	
@@ -47,7 +48,8 @@
 	<footer>
 		<button>facebook</button>
 		<button>instagram</button>
-		<button>contact</button>
+        <button>contact</button>
+        <br />
 		<span>credit</span>
 	</footer>
 </body>
