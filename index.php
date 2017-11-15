1631e44297db8eb929c3f78b4e250837b3085529
aikuaivenchorr <inarilainen@gmail.com>
2017-11-15 11:06:31 +0100

    git commit -m "jumbotron"
    Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -1,83 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
-	<title>Index Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Index Page</title>
 </head>
+
 <body>
-	<header>
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <img src="images/mh_header_photo_720.jpg">                
+            </div>
+        </div>
+    </header>
 
-	</header>
+    <!-- temporary nav -->
 
-	<!-- temporary nav -->
-	
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand hidden-md-up" href="#"><img class="logo" src="images/logo.png"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand hidden-md-up" href="#"><img class="logo" src="images/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Sunglasses</a>
-      <a class="nav-item nav-link" href="#">Watches</a>
-      <a class="nav-item nav-link" href="#">Interior</a>
-	  <a class="nav-item nav-link" href="#">Contact</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Sunglasses</a>
+                <a class="nav-item nav-link" href="#">Watches</a>
+                <a class="nav-item nav-link" href="#">Interior</a>
+                <a class="nav-item nav-link" href="#">Contact</a>
 
-	  <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     User
   </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Create new post</a>
-    <a class="dropdown-item" href="#">Profile</a>
-    <a class="dropdown-item" href="#">Help</a>
-	<a class="dropdown-item" href="#">Settings</a>
-  </div>
-</div>
-    </div>
-  </div>
-</nav>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Create new post</a>
+                        <a class="dropdown-item" href="#">Profile</a>
+                        <a class="dropdown-item" href="#">Help</a>
+                        <a class="dropdown-item" href="#">Settings</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 
 
 
-	
-	<main>
-	
-		
-		<figure>
-			<img src="" alt=""><!--3 CATEGORIES PICTURSE UNDER HERO IMAGE-->
-			<figcapture>KATEGORI</figcapture>
-		</figure>
 
-		<h1>Blogs</h1>
-		
-		
-		<article class="">
-			<button <a href="#">Name of the categorie</a></button><!--CATEGORIE TAG-->
-			<h2>BLOG TITLE</h2>
-			<figure>
-				<img src="" alt=""><!--BLOG PICTURE-->
-			</figure>
-			<p class=""></p>
-			<img src="" alt=""><!--USER IMAGE-->
-			<span></span><!--USER NAME-->
-			<time></time>
-		</article>
-	
-	
-	</main>
-	<footer>
-		<button>facebook</button>
-		<button>instagram</button>
-		<button>contact</button>
-		<span>credit</span>
-	</footer>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <main>
+
+
+        <figure>
+            <img src="" alt="">
+            <!--3 CATEGORIES PICTURSE UNDER HERO IMAGE-->
+            <figcapture>KATEGORI</figcapture>
+        </figure>
+
+        <h1>Blogs</h1>
+
+
+        <article class="">
+            <button <a href="#">Name of the categorie</a></button>
+            <!--CATEGORIE TAG-->
+            <h2>BLOG TITLE</h2>
+            <figure>
+                <img src="" alt="">
+                <!--BLOG PICTURE-->
+            </figure>
+            <p class=""></p>
+            <img src="" alt="">
+            <!--USER IMAGE-->
+            <span></span>
+            <!--USER NAME-->
+            <time></time>
+        </article>
+
+
+    </main>
+    <footer>
+        <button>facebook</button>
+        <button>instagram</button>
+        <button>contact</button>
+        <span>credit</span>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
+
 </html>
