2b5f1ffb80695363492df1f363cc8c32101d4b21
aikuaivenchorr <inarilainen@gmail.com>
2017-11-15 12:14:42 +0100

    jumbotrone and style for it

--- index.php
+++ index.php
@@ -5,17 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Index Page</title>
 </head>
 
 <body>
     <header>
         <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <img src="images/mh_header.jpg">                
-            </div>
         </div>
+        
     </header>
 
     <!-- temporary nav -->
