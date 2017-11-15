bd7eb3b0f143ad94bc23bae9dec96ca9153218b7
jdagmar <dagmar.j.gustavsson@gmail.com>
2017-11-15 20:19:04 +0100

    #5 - moved nav to seperate file

--- index.php
+++ index.php
@@ -16,33 +16,7 @@
         
     </header>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand hidden-md-up" href="index.php"><img class="logo" src="images/logo.png"></a>
-	
-		<button class="sign-up-btn d-md-none d-lg-none d-xl-none">
-			<a class="sign-up-btn__link" href="register.php">Sign up</a>
-		</button>
-
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    	<div class="navbar-nav">
-				<a class="nav-item nav-link" href="index.php">Home</a>
-				<a class="nav-item nav-link" href="categorySunglasses.php">Sunglasses</a>
-				<a class="nav-item nav-link" href="categoryWatches.php">Watches</a>
-				<a class="nav-item nav-link" href="categoryInterior.php">Interior</a>
-				<a class="nav-item nav-link" href="contact.php">Contact</a><br>
-				<a class="nav-item nav-link d-md-none d-lg-none d-xl-none" href="login.php">Login</a>
-		</div>
-		
-		<div class="user-actions d-none d-lg-block d-xl-block ml-auto">
-					<a class="user-actions__link--black" href="login.php">Login</a>
-					<button class="user-actions__sign-up-btn"><a class="user-actions__link" href="register.php">Sign up<a/></button>
-		</div>
-  </div>
-</nav>
+	<?php require 'navbar.php' ?>
 
 	<main>
 	
