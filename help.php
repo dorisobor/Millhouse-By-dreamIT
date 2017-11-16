8e2e02751f06a549703aec0536c94c4f01da55e7
jdagmar <dagmar.j.gustavsson@gmail.com>
2017-11-16 19:18:15 +0100

    added faq to help site

--- help.php
+++ help.php
@@ -4,67 +4,89 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Index Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+	<title>Millhouse blog | Help</title>
 </head>
 <body>
 
 <?php require 'navbar.php' ?>
 
-	<header>
+<main>
+	<article class="help-article-container">
+		<div class="container article__wrapper">
+				<div class="row">
+					<div class="col">
+						<div class="article__top-header">
+							<h1 class="article__heading">Frequently asked questions</h1>
+							<p class="article__text">Having any trouble? Hopefully you'll find your answers below!</p>
+						</div>
+					</div>
+				</div>
 
-	</header>
+				<div class="row">
+					<div class="col">			
+						<div class="article__content">
+							<h2 class="article__question">How do I write a post?</h2>
+							<p class="article__text">
+								Click on your username in the far end of the topmenu, choose the first option
+								"Create new post". You will also find a button for this in your profilepage.
+							</p>
+						</div>
+					</div>
+				</div>
 
+				<div class="row">
+					<div class="col">
+						<div class="article__content">					
+							<h2 class="article__question">How do I edit my posts?</h2>
+							<p class="article__text">
+								Go to the post you want to edit and press the "edit" button. You can also
+								access all your post if you click on the username in the menu and choose 
+								the second option "Profile". There you choose to edit or delete any of your
+								posts.
+							</p>
+						</div>
+					</div>
+				</div>
 
-	<main>
-	
-		<article class="article">
-			<div class="article__content">
-			<h1>FAQ</h1>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-				Totam sequi laboriosam adipisci, architecto assumenda quidem necessitatibus 
-				ipsa laudantium, excepturi ipsam cupiditate at nesciunt! Mollitia quas 
-				similique ipsam saepe eius 
-				voluptatem!</p>
+				<div class="row">
+					<div class="col">
+						<div class="article__content">
+							<h2 class="article__question">How do I change my password?</h2>
+							<p class="article__text">
+								Click on your username in the far end of the topmenu, choose the last option
+								"Settings". 
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<div class="contact">
+							<span class="contact__text">Still wondering something?</span>
+							<a href="contact.php">
+								<button class="contact__btn">Contact us!</button>
+							</a>
+						</div>
+					</div>
+				</div>	
 			</div>
+		</div> 
+	</article>	
+</main>
 
-			<div class="article__content">
-			<h2>Question?</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-				Totam sequi laboriosam adipisci, architecto assumenda quidem necessitatibus 
-				ipsa laudantium, excepturi ipsam cupiditate at nesciunt! Mollitia quas 
-				similique ipsam saepe eius 
-				voluptatem!</p>
-			</div>
+<footer>
+	<button>facebook</button>
+	<button>instagram</button>
+	<button>contact</button>
+	<span>credit</span>
+</footer>
 
-			<div class="article__content">
-			<h2>Question?</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-				Totam sequi laboriosam adipisci, architecto assumenda quidem necessitatibus 
-				ipsa laudantium, excepturi ipsam cupiditate at nesciunt! Mollitia quas 
-				similique ipsam saepe eius 
-				voluptatem!</p>
-			</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-			<div class="article__content">
-			<h2>Question?</h2>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-				Totam sequi laboriosam adipisci, architecto assumenda quidem necessitatibus 
-				ipsa laudantium, excepturi ipsam cupiditate at nesciunt! Mollitia quas 
-				similique ipsam saepe eius 
-				voluptatem!</p>
-			</div>
-			
-		</article>
-	
-	</main>
-	<footer>
-		<button>facebook</button>
-		<button>instagram</button>
-		<button>contact</button>
-		<span>credit</span>
-	</footer>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
+</html>
