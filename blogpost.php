c103e61f1f51fce5843179cb3d5d477a2c74922f
dorisobor <troyamber@live.com>
2017-11-16 14:14:54 +0100

    this is the blogpost page

--- blogpost.php
+++ blogpost.php
@@ -3,29 +3,61 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 	<title>Blogpost</title>
 </head>
 <body>
 	<header>
    </header>
-	<nav>
- <!--Menu-->
-	</nav>
+	<!-- temporary nav -->
+	
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand hidden-md-up" href="#"><img class="logo" src="images/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Sunglasses</a>
+      <a class="nav-item nav-link" href="#">Watches</a>
+      <a class="nav-item nav-link" href="#">Interior</a>
+	  <a class="nav-item nav-link" href="#">Contact</a>
+
+	  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    User
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Create new post</a>
+    <a class="dropdown-item" href="#">Profile</a>
+    <a class="dropdown-item" href="#">Help</a>
+	<a class="dropdown-item" href="#">Settings</a>
+  </div>
+</div>
+    </div>
+  </div>
+</nav>
 	<main>
 	
 		
 	 <h1>Blogpost</h1>
 		
-		<article class="">
+		<article class="blogpost">
            <!--CATEGORIE TAG-->
-			<button <a href="#">NAMN PÅ KATEGORI</a>></button>
-			<h2>BLOG TITLE</h2>
+			<button class = "blogpost__category-button"<a href="#">Category name</a>></button>
+			<h2>Blog title</h2>
 			<figure>
                 <!--BLOG PICTURE-->
-				<img src="" alt="">
+				<img src="images/inredning_kollage.jpg" alt="inredning_kollage">
 			</figure>
              <!--USER IMAGE-->
-			<p class=""></p>
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at eros dolor. Nullam sit amet velit enim. 
+			Etiam ut convallis erat. In ornare risus nec justo tincidunt, nec eleifend dolor lacinia. Curabitur ut feugiat sem,
+			 non tempus tellus. Nunc sed dolor vitae purus tristique consequat sit amet in libero. Ut rhoncus tempus justo, sit amet suscipit diam congue et. Suspendisse tempor commodo lacinia.
+             </p>
 			<img src="" alt="">
               <!--USER NAME-->
 			<span></span>
@@ -40,5 +72,8 @@
 		<button>contact</button>
 		<span>credit</span>
 	</footer>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
