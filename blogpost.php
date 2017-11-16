529e47825b41a941776b8cc191995891850b07a8
dorisobor <troyamber@live.com>
2017-11-16 16:29:58 +0100

    the blogpost page

--- blogpost.php
+++ blogpost.php
@@ -5,6 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+
 	<link rel="stylesheet" href="css/style.css">
 	<title>Blogpost</title>
 </head>
@@ -40,6 +43,7 @@
     </div>
   </div>
 </nav>
+<hr>
 	<main>
 	
 		
@@ -47,21 +51,28 @@
 		
 		<article class="blogpost">
            <!--CATEGORIE TAG-->
-			<button class = "blogpost__category-button"<a href="#">Category name</a>></button>
+			<button class = "blogpost__category-button"<a href="#">Category name</a></button>
+		 <!--USER INFO-->
+		 <div class="blogpost__user-info">
+			 		 <!--USER INFO-->
+ <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+     <span>Username</span> <time><p>Date:D/M/YYYY </p></time>
+    </div>
 			<h2>Blog title</h2>
 			<figure>
                 <!--BLOG PICTURE-->
 				<img src="images/inredning_kollage.jpg" alt="inredning_kollage">
 			</figure>
-             <!--USER IMAGE-->
-			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at eros dolor. Nullam sit amet velit enim. 
+					<div class= "blogpost__blog-description">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at eros dolor. Nullam sit amet velit enim. 
 			Etiam ut convallis erat. In ornare risus nec justo tincidunt, nec eleifend dolor lacinia. Curabitur ut feugiat sem,
-			 non tempus tellus. Nunc sed dolor vitae purus tristique consequat sit amet in libero. Ut rhoncus tempus justo, sit amet suscipit diam congue et. Suspendisse tempor commodo lacinia.
-             </p>
-			<img src="" alt="">
-              <!--USER NAME-->
-			<span></span>
-			<time></time>
+			 non tempus tellus. Nunc sed dolor vitae purus 
+			 tristique consequat sit amet in libero. Ut rhoncus tempus justo, 
+			 sit amet suscipit diam congue et. Suspendisse tempor commodo lacinia...
+             </p>   <a href="#" class="blogpost__read-more">Read More</a><br/>
+				 <div class = "blogpost__share-button"> <a href="#"> Share <i class="fa fa-share-alt" aria-hidden="true"></i></a>
+				 </div>
+						</div>
 		</article>
 	
 	
@@ -72,8 +83,8 @@
 		<button>contact</button>
 		<span>credit</span>
 	</footer>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 </body>
 </html>
