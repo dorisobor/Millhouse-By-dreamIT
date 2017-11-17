f0dc723d3c8765fb843c87b286b5ecb17fca1355
dorisobor <troyamber@live.com>
2017-11-17 11:47:40 +0100

    the index page

--- index.php
+++ index.php
@@ -13,36 +13,6 @@
 
 	</header>
 
-	<!-- temporary nav -->
-	
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand hidden-md-up" href="#"><img class="logo" src="images/logo.png"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Sunglasses</a>
-      <a class="nav-item nav-link" href="#">Watches</a>
-      <a class="nav-item nav-link" href="#">Interior</a>
-	  <a class="nav-item nav-link" href="#">Contact</a>
-
-	  <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    User
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Create new post</a>
-    <a class="dropdown-item" href="#">Profile</a>
-    <a class="dropdown-item" href="#">Help</a>
-	<a class="dropdown-item" href="#">Settings</a>
-  </div>
-</div>
-    </div>
-  </div>
-</nav>
-
 
 
 	
@@ -82,20 +52,6 @@
 				 </div>
 						</div>
 		</article>
-		<div class="paginations">
-  
-  <ul class="pagination ">
-    <li class="page-item"><a class="page-link" href="#">
-		<i class="fa fa-arrow-left" aria-hidden="true"></i>
-      </a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">
-		<i class="fa fa-arrow-right" aria-hidden="true"></i>
-  </a></li>
-  </ul>
-</div>
 	
 	</main>
 	<footer>
