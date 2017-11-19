093c57671813031e3a2f9dda512c74711a2fe6fd
wongprom <jimmybackstrom@hotmail.com>
2017-11-19 21:43:33 +0100

    removed button tags and added icons

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
     <footer>
 		<div class="footer__container">
-			<button><a href="https://www.facebook.com/">Facebook</a></button>
-			<button><a href="https://www.instagram.com/">Instagram</a></button>
-			<button><a href="/contact.php">Contact</a></button>
+		<a href="https://www.facebook.com/"><i class="fa fa-facebook-square" aria-hidden="true"></i> Facebook</a>
+		<a href="https://www.instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a>
+		<a href="/contact.php"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</a>
 			<div class="footer__credit">
 			<span>&copy;dreamIT 2017</span>
 			</div>
