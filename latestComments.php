2e3491000e685e63a43bffe081ed845ce3e96490
wongprom <jimmybackstrom@hotmail.com>
2017-11-20 11:15:55 +0100

    made some clean up in latestComments

--- latestComments.php
+++ latestComments.php
@@ -9,44 +9,16 @@
 <?php require 'logoheader.html'; ?>
 <?php require 'navbar.php' ?>
 
-	<header>
-	<div class="jumbotron jumbotron-fluid"></div>
-   </header>
+	
 	
 
 	
 
-	
-	<!--User icon as a dropdown menue-->
-		<div class="dropdown">
-  <img src="" alt="">
-  <div class="dropdown-content">
-    <a href="#">Profile |</a>
-    <a href="#">Latest Posts |</a>
-    <a href="#">Latest Comments |</a>
-  </div>
-</div>
-  
+
 
 
 	
-	<div class ="" > 
-	<!--USER IMAGE-->
-       <p class=""></p>
-		<img src="" alt="">
-		<!--edit icon from bootsrap?-->
-		<!--USER NAME-->
-		<span></span>
-		<!--settings icon from bootstrap?-->
-		</div>
-		<!--bootstrap second nav-->
-		<nav>
-		</nav>
-		<!--wrapping container-->
-		<div class ="wrapper">
-		<h2></h2>
-
-		</div> 
+	
 
 		
 		<!--latest comments-->
@@ -65,13 +37,7 @@
 	    <!--/div--> 
 	</main>
 
-	<?php require 'footer.php' ?>
 	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
-
 <?php require 'footer.php'; ?>
 <?php require 'bootstrapScripts.html'; ?>
 
