8b8a5c992d468030065f7117ae085075e97b512c
aikuaivenchorr <inarilainen@gmail.com>
2017-11-22 12:46:26 +0100

    pdo

---
+++ partials/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$pdo = new PDO(
+  "mysql:host=localhost;dbname=millhouse;charset=utf8",
+  "root",
+  ""
+);
+
+//handling of error messages
+$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//defence against simulated queries
+$pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+?>
