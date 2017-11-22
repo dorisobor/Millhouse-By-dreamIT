c7fe763b0e8d3f732da67b2e0dd887b49bcacfac
dorisobor <troyamber@live.com>
2017-11-23 00:59:45 +0100

    all blogposts printed out in the index page

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
 </head>
 
 <body>
-
-<?php require 'logoheader.html'; ?>
-<?php require 'navbar.php'; ?>
+ <!--Included files-->
+<?php require 'logoheader.html';
+      require 'partials/navbar.php';
+      require_once 'partials/db.php';
+			require_once 'partials/fetch_all_blogposts.php'; 
+			
+?>
 
 <header>
 	<div class="jumbotron jumbotron-fluid logo-header d-none d-lg-block d-xl-block"></div>    
@@ -43,38 +47,48 @@
   
 	<h1>Blogpost</h1>
 	
+<?php 
+
+//foreach to show all the blogposts
+  foreach($blogposts as $blogpost) {
+        
+?>
+   
 	<article class="blogpost">
-      <!--CATEGORIE TAG-->
-		<button class = "blogpost__category-button"<a href="#">Category name</a></button>
-		 <!--USER INFO-->
+  <!--CATEGORIE TAG-->
+		<button class = "blogpost__category-button"<a href="#">Category</a></button>
+	<!--USER INFO-->
 		<div class="blogpost__user-info">
 		<i class="fa fa-user-circle-o" aria-hidden="true"></i>
-		<span>Username</span> <time><p>Date:D/M/YYYY </p></time>
+		<span>Username</span> <time><p>Date: <?=$blogpost['postDate'];?></p></time>
 		</div>
+  	<!--The blog title-->
+		<h2><?=$blogpost['postTitle'];?></h2>
   
-		<h2>Blog title</h2>
-  
-		<!--BLOG PICTURE-->
+	<!--BLOG PICTURE-->
 		<figure>
 			<img src="images/inredning_kollage.jpg" alt="inredning_kollage">
 		</figure>
-  
-		<div class= "blogpost__blog-description">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at eros dolor. Nullam sit amet velit enim. 
-			Etiam ut convallis erat. In ornare risus nec justo tincidunt, nec eleifend dolor lacinia. Curabitur ut feugiat sem,
-				non tempus tellus. Nunc sed dolor vitae purus 
-				tristique consequat sit amet in libero. Ut rhoncus tempus justo, 
-				sit amet suscipit diam congue et. Suspendisse tempor commodo lacinia...
-			</p>
-        
-			<div class="blogpost__read-more"> <a href="#" >Read More
-				<i class="fa fa-chevron-right" aria-hidden="true"></i></a></div><br>
-	
+  <!--The blog text-->
+    <div class= "blogpost__blog-description">
+  <!--limits the text to show the first 200 characters-->
+      <a href="blogpost.php?view_post=<?=$blogpost['postID'];?>">
+			<p><?=substr ($blogpost['postText'],0,200)?>
+			...</p></a>
+         <!--a read more link-->
+     <div class="blogpost__read-more"><a href="blogpost.php?view_post=<?=$blogpost['postID'];?>">
+		 Read More<i class="fa fa-chevron-right" aria-hidden="true"></i></a></div><br>
+
 			<div class = "blogpost__share-button"> 
 				<a href="#"> Share <i class="fa fa-share-alt" aria-hidden="true"></i></a>
 			</div>
 		</div>
 	</article>
+<?php 
+
+//end of loop     
+   }
+  ?>
 </main>
 
 <?php require 'footer.php'; ?>
