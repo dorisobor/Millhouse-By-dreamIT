46309230672cf6b3c6a8e849ff581a7a832e1ad0
dorisobor <troyamber@live.com>
2017-11-23 16:25:09 +0100

    sort the posts by date

--- partials/fetch_all_blogposts.php
+++ partials/fetch_all_blogposts.php
@@ -3,8 +3,8 @@
 
 require_once 'partials/db.php';
 
-// fetch all the blogposts
-$statement = $pdo->prepare("SELECT * FROM blogposts");
+// fetch all the blogposts that will order the posts by date
+$statement = $pdo->prepare("SELECT * FROM blogposts ORDER BY postDate desc");
 
 
 $statement->execute();
