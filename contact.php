3d6d13661c302ca93605a875355cbe7c5b5ca956
Emmelie <emmelie.engstrom@gmail.com>
2017-11-23 16:23:37 +0100

    styled contact.php

--- contact.php
+++ contact.php
@@ -15,29 +15,17 @@
     <main>
         <div class="main">
             <div class="form_wrapper">
-                <h1 class="headline">Contact</h1>
-                <p class="instruction">Contact us if you have any questions or issues with the blog!</p>       
-           <div> 
-             
+                <h1>Contact</h1>
+                <p>Contact us if you have any questions or issues with the blog!</p>       
+                
                 <!-- form container -->
                 <form class="form" action="confirmation.php" method="POST">
                     <fieldset class="fieldset">
-                        <legend class="legend">Fill in your contactinformation and email us!</legend>
-                        <label class="label" for="name">Fill in your name</label><br>
-                        <input type="text" name="name" value="" placeholder="your name" required /><br>
-
-                        <label class="label" for="email">Fill in your email</label><br>
-                        <input type="text" name="email" value="" placeholder="email" required /><br>
-
-                        <label class="label" for="subject">Fill in the email subject</label><br>
+                        <input type="text" name="name" value="" placeholder="Your Name" required /><br>
+                        <input type="text" name="email" value="" placeholder="Email" required /><br>
                         <input type="text" name="subject" value="" placeholder="Subject" required /><br>
-
-                        <!-- message -->
-
-                        <label class="label" for="message"> Change post</label> <br />
-                        <textarea class="textarea" id="message" rows="6" cols="50" required></textarea><br>
-
-                        <input type="submit" value="Send" />
+                        <textarea class="textarea" id="message" rows="6" cols="50" placeholder="Write your message here..." required></textarea><br>
+                        <input type="submit" value="Send"/>
                     </fieldset>
                 </form>
 
