f7a71aad103e299f58a71de9eb7633e58eff745f
dorisobor <troyamber@live.com>
2017-11-23 01:08:36 +0100

    post printed in blogpost page

--- partials/db.php
+++ partials/db.php
@@ -1,11 +1,11 @@
 <?php
   require_once 'partials/db.php';
-?>
+
 
 $pdo = new PDO(
   "mysql:host=localhost;dbname=millhouse;charset=utf8",
   "root",
-  ""
+  "root"
 );
 
 //handling of error messages
