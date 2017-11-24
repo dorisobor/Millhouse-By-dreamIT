43f3690a6a298d6e36cfa86b83348024caff9f5c
Emmelie <emmelie.engstrom@gmail.com>
2017-11-24 16:38:34 +0100

    styled setting and contact

--- settings.php
+++ settings.php
@@ -12,21 +12,31 @@
 <?php require 'partials/navbar.php';  ?>
 
     <main>
-        <div class="form_wrapper">
-            <h1>Settings</h1>
-            <p>Change your settings here.</p>
+        <div class="mainBody">
+            <div class="form_wrapper">
+                <div class="form_heading">
+                    <h1>Settings</h1>
+                    <p>Change your settings here.</p>
+                    <br>
+                </div>
 
-            <div class="form_section">
-                <form class="form" action="" method="POST">
-                    <fieldset class="fieldset">
-                        <input type="text" id="username"  name="username" value="" placeholder="New Username" /><br>
-                        <input type="text" id="email" name="email" value="" placeholder="New Email*" /><br>
-                        <input type="text" id="email" name="email" value="" placeholder="Repeat Email*" /><br>
-                        <input type="text" id="password" name="password" value="" placeholder="New Password*" /><br>
-                        <input type="text" id="passwordConfirm" name="passwordConfirm" value="" placeholder="Confirm New Password*" /><br>
-                        <input type="submit" value="Submit" />
-                    </fieldset>
-                </form>
+                <div class="form_section">
+                    <form class="form" action="" method="POST">
+                        <fieldset class="fieldset">
+                            <div class="form_input">
+                                <input type="text" id="username"  name="username" value="" placeholder="New Username" /><br>
+                                <input type="text" id="email" name="email" value="" placeholder="New Email*" /><br>
+                                <input type="text" id="email" name="email" value="" placeholder="Repeat Email*" /><br>
+                                <input type="text" id="password" name="password" value="" placeholder="New Password*" /><br>
+                                <input type="text" id="passwordConfirm" name="passwordConfirm" value="" placeholder="Confirm New Password*" /><br>
+                            </div>
+                            
+                            <div class="form_submit">
+                                <input type="submit" value="Submit" />
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
         </div>
     </main>
