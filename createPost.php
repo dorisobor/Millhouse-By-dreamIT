a9cae5d4a7754d4cfbc21c61f4775a4f143367f0
Emmelie <emmelie.engstrom@gmail.com>
2017-11-27 11:48:52 +0100

    createPost

--- createPost.php
+++ createPost.php
@@ -13,65 +13,62 @@
 
     <main>
         <div class="main">
-            <div class="form_wrapper">
-                <div class="help">
-                    <p>
-                       <a href="help.php"><i class="fa fa-question-circle" aria-hidden="true"></i></a> 
-                    </p>
+            <div class="mainBody">
+                <div class="form_wrapper">    
+                    <form class="form" action="createPost.php" method="post" enctype="multipart/form-data">
+                        <legend class="legend"><h1>Create New Post</h1></legend>
+                        <p>Create your post here. For help press the icon.
+
+                        <div class="help">
+                            <p><a href="help.php"><i class="fa fa-question-circle" aria-hidden="true"></i></a></p>
+                        </div>
+                        <br>
+
+                        <fieldset class="fieldset">
+                            <label class="label" for="blogtitle"><p>Blog Title</p></label>
+                            <input type="text" name="blogtitle" placeholder="Title" id="headline" aria-required="true" required />
+                            <br>
+                        
+                            <label class="label" for="upload">Choose Image</label>
+                            <input type="file" name="upload" id="image" value="">
+                            <input type="hidden" name="action" value="upload"> 
+                            <br>
+                            
+                            <label class="label" for="postText">Write Post</label>
+                            <textarea class="textarea" id="postText" name="postText" rows="6" cols="50" placeholder="Write your text here" required></textarea>
+                            <br>
+
+                            <div class="formCheckboxesWrapper">
+                                <p class="form form--theme">Select one or several categories for your post:</p>
+
+                                <input type="radio" name="category" id="Sunglasses" value="Sunglasses"  required>
+                                <label class="label label--checkboxes" for="category_1">Sunglasses</label>
+
+                                <input type="radio" name="category" id="Watches" value="Watches">
+                                <label class="label label--checkboxes" for="category_2">Watches</label>
+
+                                <input type="radio" name="category" id="Interior" value="Interior">
+                                <label class="label label--checkboxes" for="category_3">Interior</label>
+                            </div>
+                        
+                            <input type="submit" value="publish" name="publish">
+                        </fieldset>
+                    </form>
                 </div>
                 
-                <form class="form" action="createPost.php" method="post" enctype="multipart/form-data">
-                    <fieldset class="fieldset">
-                        <legend class="legend"> Create a new post </legend>
-
-
-
-                        <label class="label" for="headline">Headline</label>
-                        <br />
-                        <input type="text" name="headline" placeholder="Headline" id="headline" aria-required="true" required />
-                        <br /> <br />
-
-                      
-                        <label class="label" for="upload"> Choose image </label> <br />
-                        <input type="file" name="upload" id="image" value="">
-                        <input type="hidden" name="action" value="upload"> 
-                        <br />
-                        
-                        <label class="label" for="postText"> Write post</label> <br />
-                       <textarea class="textarea" id="postText" name="postText" rows="6" cols="50" placeholder="Your text" required></textarea>
-                        <br />
-
-                     
-                        <div class="form checkboxes_wrapper">
-                            <p class="form form--theme">Select one or several categories for your post:</p>
-                            <br />
-
-
-                            <input type="radio" name="category" id="Sunglasses" value="Sunglasses"  required >
-                            <label class="label label--checkboxes" for="category_1">Sunglasses</label><br>
-                            <input type="radio" name="category" id="Watches" value="Watches">
-                            <label class="label label--checkboxes" for="category_2">Watches</label><br>
-                            <input type="radio" name="category" id="Interior" value="Interior">
-                            <label class="label label--checkboxes" for="category_3">Interior</label><br>
-                        </div>
-                      
-                        <input type="submit" value="publish" name="publish">
-                    </fieldset>
-                </form>
+    <?php 
+        var_dump($_POST);
+    if(isset($success)){
+        $imgCopyName = $_FILES['upload']['tmp_name'];
+        $imgName = $_FILES['upload']['name'];
+        $imgSize = $_FILES['upload']['size'];
+        echo  $imgCopyName . "<br>";
+        echo  $imgName;
+            echo '<br> <br> Published!<br> <script type="text/javascript">alert("Published!");</script>'; 
+        echo  $imgSize . " Kb";
+    }
+    ?>
             </div>
-            
- <?php 
-    var_dump($_POST);
-   if(isset($success)){
-       $imgCopyName = $_FILES['upload']['tmp_name'];
-       $imgName = $_FILES['upload']['name'];
-       $imgSize = $_FILES['upload']['size'];
-    echo  $imgCopyName . "<br>";
-       echo  $imgName;
-        echo '<br> <br> Published!<br> <script type="text/javascript">alert("Published!");</script>'; 
-       echo  $imgSize . " Kb";
-   }
-?>
         </div>
     </main>
 
