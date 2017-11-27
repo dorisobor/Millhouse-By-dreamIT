9286b78b800b017318d2291479196418ec1b2562
Emmelie <emmelie.engstrom@gmail.com>
2017-11-27 22:36:22 +0100

    register styling done

--- register.php
+++ register.php
@@ -3,49 +3,50 @@
 
 <head>
     <?php require 'head.html'; ?>
-  <title>Registration</title>
-</head>
-
+	<title>Register New Account</title>
 <body>
 
-<?php require 'logoheader.html'; ?>
-<?php require 'partials/navbar.php';  ?>
+    <?php require 'logoheader.html'; ?>
+    <?php require 'partials/navbar.php';  ?>
 
     <main>
         <div class="main">
-            <div class="form_wrapper">
-                <h1 class="headline">Register</h1>
-                <p class="instruction">New to Millhouse? Create your account here.</p>
+            <div class="mainBody">
+                <div class="form_wrapper">    
+                    <form class="form" action="createPost.php" method="post" enctype="multipart/form-data">
+                        <div class="topInfo">
+                            <legend class="legend"><h1>Register</h1></legend>
+                            <p>New to Millhouse? Create your account here.</p>
+                            <br>
+                        </div>
 
-                <form class="form" action="confirmation.php" method="POST">
-                    <fieldset class="fieldset">
-                        <legend class="legend">User information</legend>
-                        <label class="label" for="username">Fill in your username</label>
-                        <input type="text" id="username" name="username" value="" placeholder="username" required /><br>
+                        <fieldset class="fieldset">
+                            <div class="createForm">
+                                <input type="text" name="blogtitle" placeholder="Username*" id="headline" aria-required="true" required/>                            
+                                <input type="text" name="name" value="" placeholder="Email*" required/>
+                                <input type="text" name="email" value="" placeholder="Repeat Email*" required/>
+                                <input type="text" name="subject" value="" placeholder="Password*" required/>
+                                <input type="text" name="subject" value="" placeholder="Confirm Password*" required/>
+                            </div> 
+                                
+                            <div class="registerAgree">
+                                <input type="checkbox" name="agree" id="agree" value="yes" required>  
+                                <label class="label" for="agree">I agree to TERMS and PRIVACY POLICY</label>
+                                <br>
+                            </div>
 
-                        <label class="label" for="email">Fill in your email</label>
-                        <input type="text" id="email" name="email" value="" placeholder="Email" required /><br>
-
-                        <label class="label" for="password">Fill in your password</label>
-                        <input type="text" id="password" name="password" value="" placeholder="password" required/><br>
-
-                        <label class="label" for="passwordConfirm">Fill out password again to confirm</label>
-                        <input type="text" id="passwordConfirm" name="passwordConfirm" value="" placeholder="password" required/><br>
-
-                        <input type="checkbox" name="agree" id="agree" value="yes" required>  
-                        <label class="label" for="agree">
-                        I agree to TERMS and PRIVACY POLICY</label> <br><br>
-
-                        <input type="submit" value="Sign up" />
-                    </fieldset>
-                </form>
+                            <div class="submitButton">
+                                <input type="submit" value="Sign Up"/>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
         </div>
-
     </main>
 
-<?php require 'partials/footer.php';  ?>
-<?php require 'bootstrapScripts.html'; ?>
+    <?php require 'partials/footer.php'; ?>
+    <?php require 'bootstrapScripts.html'; ?> 
 
 </body>
 </html>
