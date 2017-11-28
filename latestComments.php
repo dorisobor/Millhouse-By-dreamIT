de6e2bc2fa164dd35077cf0cffe4dd94c3d3ccfa
jdagmar <dagmar.j.gustavsson@gmail.com>
2017-11-28 22:03:03 +0100

    if user doenst have anything published sh/e gets a message

--- latestComments.php
+++ latestComments.php
@@ -98,6 +98,17 @@ $infos = $statement->fetchAll(PDO::FETCH_ASSOC);
             </article>	
         </div>
         <?php }?>
+        <?php if (empty($info)): ?>
+			<div class="message">
+				<p class="message__if-empty">
+					You haven't commented on any story yet! 
+					Don't know where to start? 
+					<a class="message__link" href="index.php">
+						Click here to see the latest stories
+					</a>
+				</p>
+			</div>
+		<?php endif; ?>
     </div>  
 
     <!-- popup window -->
