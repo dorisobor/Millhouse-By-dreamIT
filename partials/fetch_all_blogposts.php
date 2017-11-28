6f6a5ddb13e6e1e0de5d098878410f2a71f689ba
dorisobor <troyamber@live.com>
2017-11-28 14:53:19 +0100

    updated so all the users info,post,images are fetched

--- partials/fetch_all_blogposts.php
+++ partials/fetch_all_blogposts.php
@@ -3,14 +3,19 @@
 
 require_once 'partials/db.php';
 
-// fetch all the blogposts that will order the posts by date
-$statement = $pdo->prepare("SELECT * FROM blogposts ORDER BY postDate desc");
 
+// fetch all the blogposts that will order the posts by date
+$statement = $pdo->prepare("SELECT images.* ,blogPosts.* ,categories.*  FROM blogPosts 
+JOIN categories ON categories.categoryID = blogPosts.categoryID
+LEFT JOIN images ON images.postID = blogPosts.postID
+WHERE userID = :id ORDER BY postDate DESC");
+
+$statement->bindParam(":id", $userID);    
 
 $statement->execute();
 
 
-$blogposts =  $statement ->fetchAll(PDO::FETCH_ASSOC); 
+$blogposts = $statement->fetchAll(PDO::FETCH_ASSOC); 
 
 
 ?>
