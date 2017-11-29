a74b9d3d861e6e94b537feaf20d36ef93769bd4d
dorisobor <troyamber@live.com>
2017-11-29 19:27:58 +0100

    updated the fetching in index

--- partials/fetch_all_blogposts.php
+++ partials/fetch_all_blogposts.php
@@ -2,9 +2,8 @@
 require_once 'partials/db.php';
 
 // fetch all the blogposts that will order the posts by date
-$statement = $pdo->prepare("SELECT images.* ,blogPosts.* ,users.*   FROM blogPosts 
+$statement = $pdo->prepare("SELECT blogPosts.* ,users.*   FROM blogPosts 
 JOIN users ON users.userID = blogPosts.userID
-LEFT JOIN images ON images.postID = blogPosts.postID
  ORDER BY postDate DESC");
 
 $statement->execute();
