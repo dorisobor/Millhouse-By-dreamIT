22f1fc2b918f6cbcd9dd521f64a4e450d66ae108
Emmelie <emmelie.engstrom@gmail.com>
2017-12-08 11:35:00 +0100

    category images

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ require_once 'partials/navbar.php';
     <div class="figures">
         <figure>
           <a href="pages/categoryWatches.php"> 
-          <img class ="figures__category-image" src="images/square_watch.png" alt="watches">
+          <img class ="figures__category-image" src="images/square_watch_2.png" alt="watches">
           </a><figcaption>
               <h3>
                 <a href="pages/categoryWatches.php">Watches</a>
@@ -48,7 +48,7 @@ require_once 'partials/navbar.php';
 
         <figure>
           <a href="pages/categorySunglasses.php"> 
-          <img class ="figures__category-image" src="images/square_glasses.png" alt="sunglasses">
+          <img class ="figures__category-image" src="images/square_sunglasses_2.png" alt="sunglasses">
           </a><figcaption>
             <h3>
               <a href="pages/categorySunglasses.php">Sunglasses</a>
